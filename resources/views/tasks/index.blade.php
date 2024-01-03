6965e4cc3128980bcc2ff60f9a01e5efb1f5f6ec
AizenvoltPrime <alex11737@gmail.com>
2024-01-03 10:55:06 +0200

    added tailwind and edited styles of index page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,29 +1,33 @@
 <x-layout>
-    <h1>Todo List</h1>
-        <a href="{{ route('tasks.create') }}">Add New Task</a>
-        <table class="table">
-            <thead>
+    <h1 class="text-center bg-zinc-800 p-1.5 rounded-full text-xl">Todo List</h1>
+    <div class="self-end border-2 p-1 border-slate-50 text-sm rounded-lg">
+        <a href="{{ route('tasks.create') }}">Add Task</a>
+    </div>
+    <div class="flex justify-center">
+        <table class="table-spacing min-w-full divide-y divide-gray-600 rounded-lg overflow-hidden">
+            <thead class="bg-gray-600 text-slate-200">
                 <tr>
-                    <th>ID</th>
-                    <th>Task Name</th>
-                    <th>Priority</th>
-                    <th>Completed</th>
-                    <th>Actions</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">ID</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Task Name</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Priority</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Completed</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Actions</th>
                 </tr>
             </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                    <tr>
-                        <td>{{ $task->id }}</td>
-                        <td>{{ $task->name }}</td>
-                        <td>{{ ucfirst($task->priority->value) }}</td>
-                        <td>{{ $task->completed ? 'Yes' : 'No' }}</td>
-                        <td>
-                            {{-- <a href="{{ route('tasks.edit', $task) }}">Edit</a> --}}
-                            {{-- <a href="{{ route('tasks.show', $task) }}">View</a> --}}
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
+                <tbody class="bg-gray-300 divide-y divide-gray-200 text-slate-500">
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $task->id }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $task->name }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ ucfirst($task->priority->value) }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{ $task->completed ? 'Yes' : 'No' }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                {{-- <a href="{{ route('tasks.edit', $task) }}">Edit</a> --}}
+                                        {{-- <a href="{{ route('tasks.show', $task) }}">View</a> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
         </table>
+    </div>
 </x-layout>
